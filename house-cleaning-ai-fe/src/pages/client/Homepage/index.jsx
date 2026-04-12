d6b924f385b5eb472adae560c764ad8e577961e9
NguyenDuyKhoa160104 <nduykhoa76@gmail.com>
2026-04-13 02:49:18 +0700

    Đã xong client homepage

--- house-cleaning-ai-fe/src/pages/client/Homepage/index.jsx
+++ house-cleaning-ai-fe/src/pages/client/Homepage/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./style.css";
+import "../../../assets/css/client/Homepage/index.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
